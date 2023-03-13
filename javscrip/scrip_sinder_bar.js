a1ba2ab4684056e3f5c896a24617c05bc15749e5
Olympicsys <olympicsys223@gmail.com>
2023-03-12 22:24:12 -0300

    Primeiro commit

---
+++ javscrip/scrip_sinder_bar.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    // Toggle the sidebar menu
+    $('#sidebarCollapse').on('click', function() {
+      $('#sidebar').toggleClass('active');
+    });
+  });
+  
+  
